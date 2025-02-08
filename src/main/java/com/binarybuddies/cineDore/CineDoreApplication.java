9906dcebdeef2781602e5e2419b52f5f57c21186
marlenepaper <marlenepaper@hotmail.com>
2025-02-08 15:00:14 +0100

    first commit

---
+++ src/main/java/com/binarybuddies/cineDore/CineDoreApplication.java
@@ -0,0 +1,13 @@
+package com.binarybuddies.cineDore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineDoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineDoreApplication.class, args);
+	}
+
+}
